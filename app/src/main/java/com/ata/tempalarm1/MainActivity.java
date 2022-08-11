885d6c39e7211f20a6985422fc4bbe663a063c11
Tinker42 <aabcarius@mail.csuchico.edu>
2022-08-10 23:38:57 -0700

    Added ActivityMainBinding

--- app/src/main/java/com/ata/tempalarm1/MainActivity.java
+++ app/src/main/java/com/ata/tempalarm1/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ata.tempalarm1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private ActivityMainBinding binding;
+
+
+    @Override //Overriding the AppCompactActivity which is the android class that has a function onCreate that represents the view lifecycle
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+
+
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main);
     }
 }
